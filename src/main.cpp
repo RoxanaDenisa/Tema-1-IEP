97e01dbd90390be9601684679832fdb1bce8d4f0
RoxanaDenisa <virlan_roxana99@yahoo.com>
2021-11-01 18:46:49 +0200

    Clasa Student si constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string.h>
+using namespace std;
+class Student{
+    private:
+        int varsta;
+        char *nume;
+        int nota_examen;
+    public:
+        //Constructor
+        Student(int v, int g, char *n){
+            varsta=v;
+            nota_examen=g;
+            nume=new char[strlen(n)+1];
+            strcpy(nume,n);
+             cout<<"Constructor\n";
+        }
+        
+        
+        
+};
 
 int main()
 {
-    
-    std::cout << "Tema 1" << std::endl;
+   
 
     return 0;
 }
